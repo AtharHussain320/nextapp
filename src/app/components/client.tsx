db69b92c73405c4ebc3b71ef4fe953bc1db8bc7f
AtharHussain320 <atharkeerio66320@gmail.com>
2024-11-09 00:46:39 -0800

    a

--- src/app/components/client.tsx
+++ src/app/components/client.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import React from 'react'
+
 import Card from './card'
 
 export default function Client(){
--- src/app/components/gallary.tsx
+++ src/app/components/gallary.tsx
@@ -1,5 +1,3 @@
-import { url } from "inspector"
-
 
 export default function Aallary(){
   return (
